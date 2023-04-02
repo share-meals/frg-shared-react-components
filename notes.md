a4f434a069ac79d7b3d209a50095a1ad819ef948
Jon Chin <jon@sharebyteaching.org>
2023-04-02 17:37:02 -0400

    initial commit

---
+++ notes.md
@@ -0,0 +1,13 @@
+# components to build
+- autocomplete
+  - [https://www.npmjs.com/package/@choc-ui/chakra-autocomplete](potential solution)
+- datepicker
+  - [https://github.com/aboveyunhai/chakra-dayzed-datepicker](potential solution)
+- richtext editor
+- sortable autocomplete
+  - [https://docs.dndkit.com/](potential solution)
+- query builder?
+
+# floating labels
+- [https://chakra-ui.com/community/recipes/floating-labels](community solution)
+- still WIP
---
+++ src/stories/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input as ChakraInput
+} from '@chakra-ui/react';
+
+type props_type = {
+    
+};
+
+export default function Input({
+    label,
+    helper_text
+}: props_type){
+    return (
+	<FormControl variant='floating'>
+	    {/* It is important that the Label comes after the Control due to css selectors */}
+	    <ChakraInput />
+	    {label &&
+	     <FormLabel>
+		 {label}
+	     </FormLabel>}
+	    {helper_text &&
+	     <FormHelperText>
+		 {helper_text}
+	     </FormHelperText>
+	    }
+	</FormControl>
+    );
+}
