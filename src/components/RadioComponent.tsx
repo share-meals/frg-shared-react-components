49d3b31b210ed2b3bff10752c4bd054d63a7dda0
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-18 21:54:50 -0400

    radio component and stories

---
+++ src/components/RadioComponent.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Radio as ChakraRadio,
+  RadioGroup,
+  RadioProps as ChakraRadioProps,
+  Stack,
+  StackProps,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { Controller, Control, FieldError, FieldValues } from 'react-hook-form';
+
+interface RadioOption {
+  label: string;
+  value: string;
+}
+
+export type RadioGroupProps = {
+  control: Control<FieldValues>;
+  defaultValue?: string;
+  disabled?: boolean;
+  isInvalid?: boolean;
+  options: RadioOption[];
+  stackProps?: StackProps;
+  groupLabel?: string;
+  name: string;
+  helperText?: string;
+  error?: FieldError;
+  validationRules?: any;
+} & ChakraRadioProps;
+
+const RadioComponent: React.FC<RadioGroupProps> = ({
+  control,
+  defaultValue = '',
+  disabled = false,
+  isInvalid = false,
+  options,
+  stackProps = {},
+  groupLabel,
+  name,
+  helperText,
+  error,
+  validationRules,
+  ...props
+}) => {
+  return (
+    <FormControl id={name} isInvalid={isInvalid || !!error}>
+      {groupLabel && <FormLabel>{groupLabel}</FormLabel>}
+      <Controller
+        control={control}
+        name={name}
+        defaultValue={defaultValue}
+        rules={validationRules}
+        render={({ field }) => (
+          <RadioGroup {...field} value={field.value || ''}>
+            <Stack {...stackProps}>
+              {options.map((option) => (
+                <ChakraRadio
+                  key={option.value}
+                  value={option.value}
+                  isDisabled={disabled}
+                  {...props}
+                >
+                  {option.label}
+                </ChakraRadio>
+              ))}
+            </Stack>
+          </RadioGroup>
+        )}
+      />
+      <FormErrorMessage>{error?.message || helperText}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default RadioComponent;
---
+++ src/stories/RadioComponent.stories.tsx
@@ -0,0 +1,57 @@
+// src/stories/Radio.stories.tsx
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
+import RadioComponent, { RadioGroupProps } from '../components/RadioComponent';
+import { useForm, FormProvider, FieldValues } from 'react-hook-form';
+import { Button } from '@chakra-ui/react';
+
+export default {
+  title: 'Components/Radio',
+  component: RadioComponent,
+} as Meta;
+
+const Template: Story<RadioGroupProps> = (args) => {
+  const methods = useForm<FieldValues>({
+    defaultValues: {
+      options: args.defaultValue,
+    },
+    mode: 'onSubmit',
+  });  
+  const { handleSubmit } = methods;
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <ChakraProvider>
+      <Box width="300px">
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <RadioComponent {...args} control={methods.control}/>
+            <Button mt={3} type="submit" colorScheme="blue">
+              Submit
+            </Button>
+          </form>
+        </FormProvider>
+      </Box>
+    </ChakraProvider>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  groupLabel: 'Options',
+  name: 'options',
+  options: [
+    { label: 'Option 1', value: 'option1' },
+    { label: 'Option 2', value: 'option2' },
+    { label: 'Option 3', value: 'option3' },
+  ],
+  defaultValue: 'option1',
+  helperText: 'This is helper text',
+  validationRules: {
+    required: 'This field is required',
+  },
+};
