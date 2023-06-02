3570270a5ed17a2b3ae191cba48d909292ae3f7a
Jon Chin <jon@sharemeals.org>
2023-06-02 17:08:00 -0400

    chakra is now properly imported

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path';
 import dts from 'vite-plugin-dts';
+import {peerDependencies} from './package.json';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
 	react(),
+	cssInjectedByJsPlugin(),
 	dts({
 	    insertTypesEntry: true
 	})
     ],
-    /*
     build: {
 	lib: {
 	    entry: path.resolve(__dirname, 'src/components/index.tsx'),
@@ -23,10 +25,13 @@ export default defineConfig({
 	    fileName: (format) => `frg.${format}.js`
 	},
 	rollupOptions: {
+	    /*
 	    external: [
 		'react',
 		'react-dom',
 	    ],
+	     */
+	    external: Object.keys(peerDependencies),
 	    output: {
 		globals: {
 		    react: 'React',
@@ -35,5 +40,4 @@ export default defineConfig({
 	    }
 	}
     }
-    */
 })
